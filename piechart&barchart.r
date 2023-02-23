f77dddafcdc0e591121dba511f8fe07038544207
abijith-tk <abijithtk1212@gmail.com>
2023-02-23 21:57:25 +0530

    Pie chart and barplot

---
+++ piechart&barchart.r
@@ -0,0 +1,13 @@
+# Pie chart
+
+data=c(10,10,20,40,25)
+pie(data,main='Pie chart',col=c('thistle','seashell4','midnightblue','ivory','grey28'))
+
+# Scatter plot
+x=c(1,2,3,4,5)
+y=c(2,1,4,2,5)
+plot(x,y,col='red2',main='Scatter plot',xlab='x values',ylab = 'y values')
+
+# Bar chart
+barplot(x,y,xlab = 'x values',ylab = 'y values',main='Bar chart',horiz=TRUE)
+
