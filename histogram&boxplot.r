89b8f0dd3b1b703c0db7a552053ec708e817cfad
abijith-tk <abijithtk1212@gmail.com>
2023-02-23 21:14:38 +0530

    Histogram and boxplot

--- histogram&boxplot.r
+++ histogram&boxplot.r
@@ -1,12 +1,20 @@
+# Loading iris datasets
 library(datasets)
 data(iris)
-summary(iris)
 
+# Histogram 
 hist(iris$Sepal.Length,main = "Sepal length histogram",xlab = "Sepal length",col="lightcyan3",las=1)
-colors()
+hist(iris$Petal.Length,main = "Petal length histogram",xlab = "Petal length",col="royalblue",
+     las=3,border='palegreen',breaks = 10)
 
-hist(iris$Sepal.Width,main = "Sepal width histogram",breaks=5,xlab = "Sepal width",col="paleturquoise2",las=1,border="magenta ")
-
+# Boxplot
+boxplot(iris$Sepal.Length,main='Sepal length',col='sienna2',cex.axis=1)
 dev.off()
-boxplot(iris$Sepal.Length,main = "Sepal length boxplot",xlab = "Sepal length",col="lightcyan3",las=1)
-
+boxplot(iris$Sepal.Width,main='Sepal width',col='orchid',cex.axis=1)
+dev.off()
+boxplot(iris$Petal.Length,main='Petal length',col='seagreen',cex.axis=1)
+dev.off()
+boxplot(iris$Petal.Width,main='Petal width',col='mediumblue',cex.axis=0.8)
+dev.off()
+boxplot(iris$Sepal.Length,iris$Sepal.Width,iris$Petal.Length,iris$Petal.Width,main="Box plot on iris dataset",
+        col = c('indianred2','springgreen1','wheat','seashell4'))
