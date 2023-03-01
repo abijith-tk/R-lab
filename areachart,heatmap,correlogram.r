4707c534ed230faa78729e6aaf01af72dc15fb55
abijith tk <88266300+abijith-tk@users.noreply.github.com>
2023-03-01 15:34:16 +0530

    Update areachart,heatmap,correlogram.r

--- areachart,heatmap,correlogram.r
+++ areachart,heatmap,correlogram.r
@@ -6,3 +6,23 @@ year=as.numeric(rep(seq(2017,2020),each=4))
 price=runif(16,50,100)
 data=data.frame(year,price,group)
 ggplot(data,aes(x=year,y=price,fill=group))+geom_area()
+
+# heatmap
+data=matrix(rnorm(100,0,10),nrow=10,ncol=10)
+data
+
+heatmap(data,scale = 'column')
+heatmap(data,scale = 'column',Colv = NA,Rowv = NA) # Remove dendrogram
+
+# Correlogram
+library(corrplot)
+data=matrix(rnorm(400,0,5),nrow=10,ncol=10)
+data
+cordata=cor(data)
+corrplot(cordata)
+corrplot(cor(data),method='circle')
+corrplot(cor(data),method = 'pie')
+corrplot(cor(data),method = 'number')
+corrplot(cor(data),method = 'color')
+corrplot(cor(data),type = 'upper')
+corrplot(cor(data),method = 'pie',type = 'lower')
